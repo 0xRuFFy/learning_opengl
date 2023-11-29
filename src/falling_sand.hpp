4519cfdae7f889b930cf5770741c71637e921ccc
luffy <opora.privat@gmail.com>
2023-11-29 13:34:10 +0100

    moved window/application to application/

--- src/falling_sand.hpp
+++ src/falling_sand.hpp
@@ -1,8 +1,8 @@
 
 #pragma once
 
-#include "application.hpp"
-#include "window.hpp"
+#include "application/application.hpp"
+#include "application/window.hpp"
 
 class FallingSand : public Application {
     public:
